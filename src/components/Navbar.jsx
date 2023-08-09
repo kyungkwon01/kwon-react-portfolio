007a8cd2ec77b7488aff8dae49e0d021987272da
kyungkwon01 <kyungkwon01@gmail.com>
2023-08-09 17:15:08 -0600

    second commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { FaBars, FaTimes } from 'react-icons';
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <h1>KK</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
